3cd77dac91dd9d69771dd8d4ccd25a8574a88b6d
frransier <frransier@gmail.com>
2019-06-21 23:50:46 +0200

    Images on products and product

--- site/src/pages/products.js
+++ site/src/pages/products.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import Image from "gatsby-image"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 import { mapEdgesToNodes } from "../helpers/helpers"
 
 export const query = graphql`
   {
-    products: allSanityProduct(filter: { slug: { current: { ne: null } } }) {
+    products: allSanityProduct {
       edges {
         node {
           id
@@ -17,6 +18,15 @@ export const query = graphql`
           slug {
             current
           }
+          defaultProductVariant {
+            images {
+              asset {
+                fixed(height: 70, width: 70) {
+                  ...GatsbySanityImageFixed
+                }
+              }
+            }
+          }
         }
       }
     }
@@ -26,16 +36,29 @@ const ProductsPage = props => {
   const { data } = props
 
   const productNodes = mapEdgesToNodes(data.products)
+  console.log(productNodes[0])
 
   return (
     <Layout>
       <SEO title="Products" />
       {productNodes.map(product => (
         <div>
-          <Link to={`products/${product.slug.current}`}>{product.title}</Link>
-          {product.categories.map(category => (
-            <div>{category.title}</div>
-          ))}
+          <div>
+            <Image
+              fixed={product.defaultProductVariant.images[0].asset.fixed}
+            />
+            <Link to={`products/${product.slug.current}`}>{product.title}</Link>
+            <br />
+            <div>
+              Categories:
+              {product.categories.map(category => (
+                <div>
+                  <div>{category.title}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <br />
         </div>
       ))}
     </Layout>
--- site/src/templates/product.js
+++ site/src/templates/product.js
@@ -1,12 +1,24 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Image from "gatsby-image"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
 
 export const query = graphql`
   query ProductQuery($id: String!) {
     product: sanityProduct(id: { eq: $id }) {
+      id
       title
+      defaultProductVariant {
+        price
+        images {
+          asset {
+            fixed {
+              ...GatsbySanityImageFixed
+            }
+          }
+        }
+      }
     }
   }
 `
@@ -18,7 +30,13 @@ const ProductTemplate = props => {
   return (
     <Layout>
       {product && <SEO title={product.title} />}
-      {product && <div>{product.title}</div>}
+      {product && (
+        <div>
+          <div>{product.title}</div>
+          <div>{product.defaultProductVariant.price} kr</div>
+          <Image fixed={product.defaultProductVariant.images[0].asset.fixed} />
+        </div>
+      )}
     </Layout>
   )
 }
