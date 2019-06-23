0f5a93b091a4e6dfe34e33819fe39eb103ce8fe7
frransier <frransier@gmail.com>
2019-06-23 21:59:16 +0200

    contact page with netlify form

---
+++ site/src/pages/contact.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => {
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
+
+  function handleEmail(event) {
+    setEmail(event.target.value)
+  }
+
+  function handleMessage(event) {
+    setMessage(event.target.value)
+  }
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <form
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="yekshemesh"
+      >
+        <div>
+          <input type="hidden" name="form-name" value="contact" />
+          <input onChange={handleEmail} name="Email" required />
+          <input onChange={handleMessage} name="Message" required />
+          <input type="hidden" name="yekshemesh" />
+
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Layout>
+  )
+}
+
+export default ContactPage
