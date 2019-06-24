d99c2a53232c0dbc9f958150afeb9ebbe9bbfed5
frransier <frransier@gmail.com>
2019-06-24 20:36:57 +0200

    algolia plugin

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -2,6 +2,36 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const algoliaQuery = `
+{
+  allSanityProduct {
+    edges {
+      node {
+        title
+        slug {
+          current
+        }
+        tags
+        categories {
+          title
+          slug {
+            current
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const queries = [
+  {
+    query: algoliaQuery,
+    transformer: ({ data }) =>
+      data.allSanityProduct.edges.map(({ node }) => node),
+  },
+]
+
 module.exports = {
   siteMetadata: {
     title: `Static ecommerce poc`,
@@ -9,6 +39,16 @@ module.exports = {
     author: `@wipcore`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.ALGOLIA_APP,
+        apiKey: process.env.ALGOLIA_KEY,
+        indexName: process.env.ALGOLIA_INDEX, // for all queries
+        queries,
+        chunkSize: 10000, // default: 1000
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
