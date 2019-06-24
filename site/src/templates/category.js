734f205fa475173d026879837b1974a294c4fcf9
WIPCORE\mikael.karlsson <mikael.karlsson@wipcore.se>
2019-06-24 16:37:45 +0200

    List category data properly

--- site/src/templates/category.js
+++ site/src/templates/category.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-import Image from "gatsby-image"
 import SEO from "../components/seo"
 import Layout from "../components/layout"
+import { mapEdgesToNodes } from "../helpers/helpers"
 
 export const query = graphql`
-  query ProductsQuery($id: String!) {
+  query CategoryQuery($id: String!) {
+    category: sanityCategory(id: {eq: $id}) {
+      title
+      description
+    }
     products: allSanityProduct(filter: {categories: {elemMatch: {id: {eq: $id}}}}) {
       edges {
         node {
@@ -27,11 +31,29 @@ export const query = graphql`
 
 const CategoryTemplate = props => {
   const { data } = props
+  const category = data && data.category
   const products = data && data.products
+  const productNodes = mapEdgesToNodes(products)
 
   return (
     <Layout>
-      <p>{JSON.stringify(products)}</p>
+      <SEO title={category.title} />
+      <h1>{category.title}</h1>
+      <div>
+        {category.description}
+      </div>
+      <div>
+        Antal produkter: {products.totalCount}
+        <ul>
+          {productNodes.map(product => (
+            <li key={product.id}>
+              <Link to={`/products/${product.slug.current}`}>
+                {product.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   )
 }
