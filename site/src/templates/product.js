e44381f8f24a774102ae67b8fd97421238e41043
frransier <frransier@gmail.com>
2019-06-26 10:59:24 +0200

    getting price dynamically from sanity

--- site/src/templates/product.js
+++ site/src/templates/product.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import { useGraphQL } from "../utils/use-graphql"
 import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
 import SEO from "../components/seo"
@@ -7,7 +8,7 @@ import Layout from "../components/layout"
 export const query = graphql`
   query ProductQuery($id: String!) {
     product: sanityProduct(id: { eq: $id }) {
-      id
+      _id
       title
       slug {
         current
@@ -29,7 +30,32 @@ export const query = graphql`
 
 const ProductTemplate = props => {
   const { data } = props
+
   const product = data && data.product
+
+  const [id] = useState(product._id)
+
+  const { result, loading, error } = useGraphQL(
+    "https://39k0k3q1.api.sanity.io/v1/graphql/production/default",
+    `query 
+        Product($id: ID!) {
+            Product(id:$id){
+                defaultProductVariant {
+                    price
+                }
+            }
+        }
+      
+    `,
+    { id }
+  )
+
+  if (loading) {
+    return <div>GETTING PRICE FROM SANITY</div>
+  }
+  if (error) {
+    return <div>Error getting graphql data</div>
+  }
 
   return (
     <Layout>
@@ -38,18 +64,19 @@ const ProductTemplate = props => {
       {product && (
         <div>
           <div>{product.title}</div>
+
+          <Image fixed={product.defaultProductVariant.images[0].asset.fixed} />
+
           <button
             className="snipcart-add-item"
-            data-item-id={product.id}
-            data-item-price={product.defaultProductVariant.price}
+            data-item-id={product._id}
+            data-item-price={result.Product.defaultProductVariant.price}
             data-item-image={product.defaultProductVariant.images[0].asset.url}
             data-item-name={product.title}
             data-item-url={`http://static-ecommerce-poc.netlify.com/products/${product.slug.current}/`}
           >
-            Buy now for {product.defaultProductVariant.price} kronor
+            Buy now for kronor {result.Product.defaultProductVariant.price}
           </button>
-
-          <Image fixed={product.defaultProductVariant.images[0].asset.fixed} />
         </div>
       )}
     </Layout>
