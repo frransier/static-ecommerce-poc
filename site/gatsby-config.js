9f1a4724b1c132d15a7e5725737ef38d1b0a9c76
frransier <frransier@gmail.com>
2019-06-29 11:21:43 +0200

    better pwa

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -24,12 +24,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `static-ecommerce-poc`,
+        short_name: `Wipcore`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
