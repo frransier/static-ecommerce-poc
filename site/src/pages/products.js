b62361c0188fa2aeb4c9fb25ee04061f1e998ae2
frransier <frransier@gmail.com>
2019-06-29 11:21:29 +0200

    cleanup search

--- site/src/pages/products.js
+++ site/src/pages/products.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "gatsby"
 
 import SEO from "../components/seo"
@@ -22,48 +22,47 @@ const searchClient = algoliasearch(
   "3a599a08fde10c670966018cd5db6b2a"
 )
 
-const Sidebar = () => (
-  <div>
-    <h3>Filter by categories</h3>
-    <RefinementList attribute="categories.title"></RefinementList>
-  </div>
-)
-
 const ProductsPage = () => {
+  const [didMount, setDidMount] = useState(false)
+
+  useEffect(() => {
+    setDidMount(true)
+  }, [])
+
   return (
     <Layout>
       <SEO title="Products" />
       <Link to="/">Back</Link>
 
-      <InstantSearch
-        searchClient={searchClient}
-        indexName="static-ecommerce-poc"
-      >
-        <aside>
-          <Sidebar></Sidebar>
-        </aside>
-        <SortBy
-          defaultRefinement="static-ecommerce-poc"
-          items={[
-            { value: "static-ecommerce-poc", label: "Most relevant" },
-            { value: "static-ecommerce-poc-price-asc", label: "Price asc" },
-            { value: "static-ecommerce-poc-price-desc", label: "Price desc" },
-          ]}
-        ></SortBy>
-        <HitsPerPage
-          defaultRefinement={5}
-          items={[
-            { value: 1, label: "Show 1 hit" },
-            { value: 5, label: "Show 5 hits" },
-            { value: 10, label: "Show 10 hits" },
-          ]}
-        />
-        <SearchBox />
-        <Stats />
-        <Hits hitComponent={ProductPreview} />
+      {didMount ? (
+        <InstantSearch
+          searchClient={searchClient}
+          indexName="static-ecommerce-poc"
+        >
+          <RefinementList attribute="categories.title"></RefinementList>
+          <SortBy
+            defaultRefinement="static-ecommerce-poc"
+            items={[
+              { value: "static-ecommerce-poc", label: "Most relevant" },
+              { value: "static-ecommerce-poc-price-asc", label: "Price asc" },
+              { value: "static-ecommerce-poc-price-desc", label: "Price desc" },
+            ]}
+          ></SortBy>
+          <HitsPerPage
+            defaultRefinement={5}
+            items={[
+              { value: 1, label: "Show 1 hit" },
+              { value: 5, label: "Show 5 hits" },
+              { value: 10, label: "Show 10 hits" },
+            ]}
+          />
+          <SearchBox />
+          <Stats />
+          <Hits hitComponent={ProductPreview} />
 
-        <Pagination showLast></Pagination>
-      </InstantSearch>
+          <Pagination showLast></Pagination>
+        </InstantSearch>
+      ) : null}
     </Layout>
   )
 }
