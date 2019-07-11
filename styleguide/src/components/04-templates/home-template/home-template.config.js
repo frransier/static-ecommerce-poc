80d287dfe46843a07d5b0920f6a70210b25a594a
WIPCORE\mikael.karlsson <mikael.karlsson@wipcore.se>
2019-07-11 11:50:59 +0200

    Resolved all faulty variables so the CSS can build

---
+++ styleguide/src/components/04-templates/home-template/home-template.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'home-template',
+    status: 'ready',
+    preview: '@master--offset',
+    context: {
+        imageSliderContext: {},
+        sections: {
+            newProducts: {
+                header: {
+                    h2: 'Nyheter'
+                }
+            },
+            manufacturerProducts: {
+                header: {
+                    h2: 'Utvalt från Garmin'
+                }
+            },
+            manufacturerList: {
+                header: {
+                    h2: 'Utvalda varumärken'
+                }
+            },
+            cardsList: {
+                header: {
+                    h2: 'Reportage'
+                }
+            },
+            equipmentProducts: {
+                header: {
+                    h2: 'Utrustning för jakten'
+                }
+            }
+        }
+    }
+}
