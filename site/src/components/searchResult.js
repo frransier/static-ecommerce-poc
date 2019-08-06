18a1ef3d06215a5d57bf945f32669d74e7c059d2
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2019-08-06 14:17:33 +0200

    Show total hits in search result

--- site/src/components/searchResult.js
+++ site/src/components/searchResult.js
@@ -4,47 +4,52 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 import { connectHits, Highlight } from "react-instantsearch-dom"
 
 const SearchResult = ({ hits }) => (
-  <ul className="search-result__list">
-    {hits.map(hit => (
-      <li className="search-result__item" key={hit.objectID}>
-        <AniLink
-          to={hit.slug ? `/products/${hit.slug.current}` : "/"}
-          className="search-article"
-        >
-          <div className="search-article__image-container">
-            <img
-              src={hit.asset.fixed.srcWebp}
-              alt={hit.title}
-              className="search-article__image"
-            />
-          </div>
-          <div className="search-article__name-container">
-            <div className="product-name product-name--break">
-              <span className="product-name__brand">
-                {hit["categories.lvl1"]}
-              </span>
-              <h2 className="product-name__name product-name__name--inherit-size">
-                <Highlight hit={hit} attribute="title" tagName="mark" />
-              </h2>
-            </div>
-            <span className="stock-status stock-status--few-in-stock stock-status--discrete">
-              <span className="stock-status__text">Få i lager</span>
-            </span>
-          </div>
-          <div className="search-article__details-container">
-            <div className="search-article__details-placement">
-              <div className="product-price h-flex-align-end">
-                <strong className="product-price__discounted">249:-</strong>
-                <span className="product-price__regular product-price__regular--is-not-current">
-                  Ord. 500:-
+  <div className="search__result-scroll">
+    <div className="search-result">
+      <h4 className="search-result__heading">Sortiment ({hits.length})</h4>
+      <ul className="search-result__list">
+        {hits.map(hit => (
+          <li className="search-result__item" key={hit.objectID}>
+            <AniLink
+              to={hit.slug ? `/products/${hit.slug.current}` : "/"}
+              className="search-article"
+            >
+              <div className="search-article__image-container">
+                <img
+                  src={hit.asset.fixed.srcWebp}
+                  alt={hit.title}
+                  className="search-article__image"
+                />
+              </div>
+              <div className="search-article__name-container">
+                <div className="product-name product-name--break">
+                  <span className="product-name__brand">
+                    {hit["categories.lvl1"]}
+                  </span>
+                  <h2 className="product-name__name product-name__name--inherit-size">
+                    <Highlight hit={hit} attribute="title" tagName="mark" />
+                  </h2>
+                </div>
+                <span className="stock-status stock-status--few-in-stock stock-status--discrete">
+                  <span className="stock-status__text">Få i lager</span>
                 </span>
               </div>
-            </div>
-          </div>
-        </AniLink>
-      </li>
-    ))}
-  </ul>
+              <div className="search-article__details-container">
+                <div className="search-article__details-placement">
+                  <div className="product-price h-flex-align-end">
+                    <strong className="product-price__discounted">249:-</strong>
+                    <span className="product-price__regular product-price__regular--is-not-current">
+                      Ord. 500:-
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </AniLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
 )
 
 export default connectHits(SearchResult)
