6b93ef721fd9ea44bbda19b6bd1160f7822a13fa
Sebastian Gelotte <sebastian.gelotte@gmail.com>
2019-08-06 11:50:29 +0200

    Autofocus search input

--- site/src/components/search.js
+++ site/src/components/search.js
@@ -1,16 +1,20 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, createRef } from "react"
 import { LayoutContext } from "../context/LayoutStore"
 
 import { connectSearchBox } from "react-instantsearch-dom"
 import SearchResult from "./searchResult"
 
-const Search = ({ refine }) => {
+const Search = ({ refine, currentRefinement }) => {
   const [state] = useContext(LayoutContext)
 
-  const handleSearch = event => {
-    const searchValue = event.currentTarget.value
+  // Focus the input element when it's shown
+  const inputRef = createRef()
+  useEffect(() => {
+    inputRef.current.focus()
+  })
 
-    refine(searchValue)
+  const handleSearch = event => {
+    refine(event.currentTarget.value)
   }
 
   return (
@@ -22,6 +26,7 @@ const Search = ({ refine }) => {
       <div className="search__inner">
         <div className="search__field-container">
           <input
+            ref={inputRef}
             type="search"
             className="input search__field-input"
             name="searchfield"
@@ -29,7 +34,6 @@ const Search = ({ refine }) => {
             tabIndex="-1"
             onChange={handleSearch}
           />
-
           <button
             className="button button--red search__button"
             tabIndex="-1"
@@ -38,29 +42,34 @@ const Search = ({ refine }) => {
             Sök
           </button>
         </div>
-        <div className="search__result-container search__result-container--is-expanded">
-          <div className="search__result-scroll">
-            <div className="search-result">
-              <h4 className="search-result__heading">Sortiment (666)</h4>
-              <SearchResult />
+        {currentRefinement && (
+          <div className="search__result-container search__result-container--is-expanded">
+            <div className="search__result-scroll">
+              <div className="search-result">
+                <h4 className="search-result__heading">Sortiment (666)</h4>
+                <SearchResult />
+              </div>
             </div>
-          </div>
-          <a
-            className="button button--is-link button--red button--text-center button-icon"
-            href="/products"
-            tabIndex="-1"
-          >
-            <svg className="icon icon--xs button-icon__icon" aria-hidden="true">
-              <use
-                xmlnsXlink="http://www.w3.org/1999/xlink"
-                xlinkHref="../../content/assets/icons/icon-sprite.svg#double-chevron"
-              ></use>
-            </svg>
-            <span className="hide-visually">SR only text</span>
+            <a
+              className="button button--is-link button--red button--text-center button-icon"
+              href="/products"
+              tabIndex="-1"
+            >
+              <svg
+                className="icon icon--xs button-icon__icon"
+                aria-hidden="true"
+              >
+                <use
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  xlinkHref="../../content/assets/icons/icon-sprite.svg#double-chevron"
+                ></use>
+              </svg>
+              <span className="hide-visually">SR only text</span>
 
-            <span className="button-icon__text">Visa sökresultat</span>
-          </a>
-        </div>
+              <span className="button-icon__text">Visa sökresultat</span>
+            </a>
+          </div>
+        )}
       </div>
     </div>
   )
