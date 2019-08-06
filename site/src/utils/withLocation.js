1e028a36e3e35fbae846349c5b90d3d214431914
frransier <frransier@gmail.com>
2019-08-07 00:04:48 +0200

    a really ugly check to window === undefined

--- site/src/utils/withLocation.js
+++ site/src/utils/withLocation.js
@@ -2,20 +2,25 @@ import React from "react"
 import { createHistory, Location } from "@reach/router"
 import queryString from "query-string"
 
-const history = createHistory(window)
+const history = typeof window !== "undefined" ? createHistory(window) : "bajjs"
 
-const withLocation = ComponentToWrap => props => (
-  <Location>
-    {({ location, navigate }) => (
-      <ComponentToWrap
-        {...props}
-        location={location}
-        navigate={navigate}
-        search={location.search ? queryString.parse(location.search) : {}}
-        history={history}
-      />
-    )}
-  </Location>
-)
+const withLocation = ComponentToWrap => props => {
+  if (typeof window !== "undefined") {
+    return (
+      <Location>
+        {({ location, navigate }) => (
+          <ComponentToWrap
+            {...props}
+            location={location}
+            navigate={navigate}
+            search={location.search ? queryString.parse(location.search) : {}}
+            history={history}
+          />
+        )}
+      </Location>
+    )
+  }
+  return <ComponentToWrap></ComponentToWrap>
+}
 
 export default withLocation
