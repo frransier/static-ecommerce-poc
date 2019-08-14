da3431c640e004419f28f06cc49c0a8617e6cd19
mravand <mattias.ravand@wipcore.se>
2019-08-14 12:01:11 +0200

    acknowledge klarna order

--- site/functions/klarna/klarna.js
+++ site/functions/klarna/klarna.js
@@ -4,35 +4,63 @@ exports.handler = (event, context, callback) => {
   console.log("heres what we get in event: ", event)
   console.log("heres what we get in context: ", context)
 
-  const boo = JSON.parse(event.body)
-  const data = boo.params
-
   const sanity = sanityClient({
     projectId: process.env.SANITY_ID,
     dataset: process.env.SANITY_DATASET,
     token: process.env.SANITY_WRITE,
     useCdn: false,
   })
-
-  const items = data.order_lines.map(item => {
-    const i = item.reference
-    return i
-  })
-
-  const doc = {
-    _type: "order",
-    name: data.billing_address.given_name + data.billing_address.family_name,
-    email: data.billing_address.email,
-    orderDate: data.completed_at,
-    orderItems: items,
-    total: data.order_amount / 100,
-    status: data.status,
-    acknowledged: false,
-    orderId: data.order_id,
-  }
-
   try {
-    sanity.create(doc)
+    if (event.queryStringParameters) {
+      const Username = "PK04103_3d21aa53e7a6"
+      const Password = "MD2ifgWSytidwwUV"
+      const config = {
+        headers: {
+          Authorization: "Basic " + btoa(`${Username}:${Password}`),
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+        },
+      }
+      const PROXY_URL = "https://cors-anywhere.herokuapp.com/"
+
+      const targetUrl = `https://api.playground.klarna.com/ordermanagement/v1/orders/${event.queryStringParameters.klarna_order_id}/acknowledge`
+
+      const acknowledeKlarnaOrder = () => {
+        axios.post(PROXY_URL + targetUrl, config)
+      }
+      sanity
+        .patch(event.queryStringParameters.klarna_order_id)
+        .set({ acknowledged: true })
+        .commit()
+        .then(updated => {
+          console.log("Order acknowledged: ", updated)
+        })
+        .finally(acknowledeKlarnaOrder())
+    } else {
+      const boo = JSON.parse(event.body)
+      const data = boo.params
+
+      const items = data.order_lines.map(item => {
+        const i = item.reference
+        return i
+      })
+
+      const doc = {
+        _type: "order",
+        _id: data.order_id,
+        _key: data.order_id,
+        name:
+          data.billing_address.given_name + data.billing_address.family_name,
+        email: data.billing_address.email,
+        orderDate: data.completed_at,
+        orderItems: items,
+        total: data.order_amount / 100,
+        status: data.status,
+        acknowledged: false,
+        orderId: data.order_id,
+      }
+      sanity.create(doc)
+    }
 
     callback(null, {
       statusCode: 200,
