fd99917cab8f82b57e4e2b076659009b73c6566c
frransier <frransier@gmail.com>
2019-08-15 20:20:58 +0200

    ueh

--- site/functions/klarna-push/klarna-push.js
+++ site/functions/klarna-push/klarna-push.js
@@ -1,37 +1,37 @@
-const sanityClient = require("@sanity/client")
+//const sanityClient = require("@sanity/client")
 const btoa = require("btoa")
 
 exports.handler = (event, context, callback) => {
-  const sanity = sanityClient({
-    projectId: process.env.SANITY_ID,
-    dataset: process.env.SANITY_DATASET,
-    token: process.env.SANITY_WRITE,
-    useCdn: false,
-  })
+  //   const sanity = sanityClient({
+  //     projectId: process.env.SANITY_ID,
+  //     dataset: process.env.SANITY_DATASET,
+  //     token: process.env.SANITY_WRITE,
+  //     useCdn: false,
+  //   })
 
+  const Username = "PK04103_3d21aa53e7a6"
+  const Password = "MD2ifgWSytidwwUV"
+  const config = {
+    headers: {
+      Authorization: "Basic " + btoa(`${Username}:${Password}`),
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+  }
+  const PROXY_URL = "https://cors-anywhere.herokuapp.com/"
+  const pushUrl = `https://api.playground.klarna.com/ordermanagement/v1/orders/${event.queryStringParameters.klarna_order_id}/acknowledge`
   try {
-    sanity
-      .patch(event.queryStringParameters.klarna_order_id)
-      .set({ acknowledged: true })
-      .commit()
-      .then(updated => {
-        console.log("Order acknowledged: ", updated)
-      })
-    // const Username = "PK04103_3d21aa53e7a6"
-    // const Password = "MD2ifgWSytidwwUV"
-    // const config = {
-    //   headers: {
-    //     Authorization: "Basic " + btoa(`${Username}:${Password}`),
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Content-Type": "application/json",
-    //   },
-    // }
-    // const PROXY_URL = "https://cors-anywhere.herokuapp.com/"
-    // const pushUrl = `https://api.playground.klarna.com/ordermanagement/v1/orders/${klarna_order_id}/acknowledge`
-    // axios
-    //   .post(PROXY_URL + pushUrl, config)
-    //   .then(res => console.log("post:", res))
-    //   .catch(err => console.log("Post ERROR :", err))
+    // sanity
+    //   .patch(event.queryStringParameters.klarna_order_id)
+    //   .set({ acknowledged: true })
+    //   .commit()
+    //   .then(updated => {
+    //     console.log("Order acknowledged: ", updated)
+    //   })
+    axios
+      .post(PROXY_URL + pushUrl, config)
+      .then(res => console.log("post:", res))
+      .catch(err => console.log("Post ERROR :", err))
 
     callback(null, {
       statusCode: 200,
