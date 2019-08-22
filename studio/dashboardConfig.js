01b95f5bcad4987d8df36bf707e67cab558bdb65
mravand <mattias.ravand@wipcore.se>
2019-08-22 14:16:01 +0200

    not super awesome but still a dashboard

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,33 @@
+export default {
+  widgets: [
+    {
+      name: "document-list",
+      options: {
+        title: "Latest items",
+        order: "_createdAt desc",
+        types: ["product", "variant", "page", "story", "news"],
+      },
+      layout: { width: "medium", height: "large" },
+    },
+    {
+      name: "netlify",
+      layout: {
+        width: "small",
+        height: "small",
+      },
+      options: {
+        title: "My Netlify deploys",
+        sites: [
+          {
+            title: "Live site",
+            apiId: "f6f724d9-0f2a-4877-b11b-6a8c3eb7d879",
+            buildHookId: "5d5d54d1d2170d2924901151",
+            name: "static-ecommerce-poc",
+          },
+        ],
+      },
+    },
+    { name: "minesweeper" },
+    { name: "notes" },
+  ],
+};
